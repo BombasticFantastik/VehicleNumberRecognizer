ee9a1dae312c93f43082b2a79277cc8df6c1c577
ArtemyBombastic <artemilusenko2@gmail.com>
2026-01-14 22:27:37 +0700

    добавил управление запуска файлов из файла main.py с помощью 'флагов'

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import click
 @click.command()
 @click.argument("option")
 @click.option("--option", "-o", default='train', help="Режим работы, train или eval")
+@click.help_option('--help','-h',help='Показывает инструкции')
 def hello(option):
-    """Меню позволяющее выбрать режим программы, eval """
+    """Меню позволяющее выбрать режим программы"""
     match option:
         case 'train':
-            pass
+            print(20*'_')
+            print('ЗАПУСКА ОБУЧЕНИЯ')
+            print(20*'_')
+            import train
         case 'eval':
-            pass
+            print(20*'_')
+            print('ЗАПУСК ТЕСТИРОВАНИЯ')
+            print(20*'_')
+            import eval
     click.echo(f"Выбран {option} режим!")
 
 if __name__ == "__main__":
